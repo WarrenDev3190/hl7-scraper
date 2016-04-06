020672dd9ef43a93879902392cdbee7cc945544a
WarrenDev3190 <warrendev3190@gmail.com>
2016-04-06 08:56:39 -0500

    Print-str to pr-str, hook to main in proj.clj

--- src/hl7_scraper/core.clj
+++ src/hl7_scraper/core.clj
@@ -1,6 +1,6 @@
 (ns hl7-scraper.core
   (:require [hl7-scraper.hl7-helpers :as helpers]))
 
-(defn main- []
+(defn -main []
   (spit "hl7.edn"
-    (print-str (helpers/retrieve-hl7))))
+    (pr-str (helpers/retrieve-hl7))))
